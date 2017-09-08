ce166bcef939bb000cd7567c9bf1985d0b48bb40
Kamil Ciesielski <kamil.ciesielski1@pl.sii.eu>
2017-09-08 10:42:21 +0200

    AB entities

---
+++ src/AppBundle/Entity/AB/Email.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity\AB;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Email
+ *
+ * @ORM\Table(name="email")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EmailRepository")
+ */
+class Email
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email2", type="string", length=255, nullable=true)
+     */
+    private $email2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="workEmail", type="string", length=255, nullable=true)
+     */
+    private $workEmail;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set email2
+     *
+     * @param string $email2
+     *
+     * @return Email
+     */
+    public function setEmail2($email2)
+    {
+        $this->email2 = $email2;
+
+        return $this;
+    }
+
+    /**
+     * Get email2
+     *
+     * @return string
+     */
+    public function getEmail2()
+    {
+        return $this->email2;
+    }
+
+    /**
+     * Set workEmail
+     *
+     * @param string $workEmail
+     *
+     * @return Email
+     */
+    public function setWorkEmail($workEmail)
+    {
+        $this->workEmail = $workEmail;
+
+        return $this;
+    }
+
+    /**
+     * Get workEmail
+     *
+     * @return string
+     */
+    public function getWorkEmail()
+    {
+        return $this->workEmail;
+    }
+}
+
---
+++ src/AppBundle/Entity/AB/Telephone.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Telephone
+ *
+ * @ORM\Table(name="telephone")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TelephoneRepository")
+ */
+class Telephone
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mobileNo", type="string", length=100, nullable=true)
+     */
+    private $mobileNo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="homeNo", type="string", length=100, nullable=true)
+     */
+    private $homeNo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="workNo", type="string", length=100, nullable=true)
+     */
+    private $workNo;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set mobileNo
+     *
+     * @param string $mobileNo
+     *
+     * @return Telephone
+     */
+    public function setMobileNo($mobileNo)
+    {
+        $this->mobileNo = $mobileNo;
+
+        return $this;
+    }
+
+    /**
+     * Get mobileNo
+     *
+     * @return string
+     */
+    public function getMobileNo()
+    {
+        return $this->mobileNo;
+    }
+
+    /**
+     * Set homeNo
+     *
+     * @param string $homeNo
+     *
+     * @return Telephone
+     */
+    public function setHomeNo($homeNo)
+    {
+        $this->homeNo = $homeNo;
+
+        return $this;
+    }
+
+    /**
+     * Get homeNo
+     *
+     * @return string
+     */
+    public function getHomeNo()
+    {
+        return $this->homeNo;
+    }
+
+    /**
+     * Set workNo
+     *
+     * @param string $workNo
+     *
+     * @return Telephone
+     */
+    public function setWorkNo($workNo)
+    {
+        $this->workNo = $workNo;
+
+        return $this;
+    }
+
+    /**
+     * Get workNo
+     *
+     * @return string
+     */
+    public function getWorkNo()
+    {
+        return $this->workNo;
+    }
+}
+
